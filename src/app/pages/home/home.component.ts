0f514219fba0babfb55d1ede54ccf5b5f3bd17ef
NDRBAX <andre.bacaliu@icloud.com>
2024-11-12 10:59:35 +0100

    feat: Add medals data processing and chart configuration to HomeComponent
    - Added 'Participation' model import to handle Olympic participation
    data
    - Introduced 'medalsData' property in HomeComponent to store processed
    medal counts
    - Implemented logic to calculate total medals for each country based on
    Olympic participations
    - Subscribed to olympics$ observable to populate medalsData with
    aggregated medal counts
    - Added chart view dimensions and color scheme configuration for future
    medal chart rendering

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { catchError, delay, map, Observable, of } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
+import { Participation } from 'src/app/core/models/Participation';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -10,6 +11,10 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$!: Observable<Olympic[]>;
+  public medalsData!: {
+    name: string;
+    value: number;
+  }[];
 
   constructor(private olympicService: OlympicService) {}
 
@@ -19,5 +24,24 @@ export class HomeComponent implements OnInit {
       map((data) => data || []), // Si data est null ou undefined, retourne un tableau vide
       catchError(() => of([])) // En cas d'erreur, retourne un tableau vide
     );
+
+    this.olympics$.subscribe((olympics: Olympic[]) => {
+      this.medalsData = olympics.map((olympic: Olympic) => {
+        return {
+          name: olympic.country,
+          value: olympic.participations.reduce(
+            (acc: number, participation: Participation) =>
+              acc + participation.medalsCount,
+            0
+          ),
+        };
+      });
+    });
   }
+
+  view: [number, number] = [700, 400];
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA', '#FF8C00'],
+  };
 }
