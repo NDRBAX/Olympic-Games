a5e079d67429e345d403f66e49be9be59a6e9656
NDRBAX <andre.bacaliu@icloud.com>
2024-11-12 17:09:21 +0100

    Docs added

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -13,9 +13,16 @@ export class OlympicService {
 
   constructor(private http: HttpClient) {}
 
+  /**
+   * Charge les données initiales des Jeux Olympiques depuis le fichier JSON.
+   * Utilise un appel HTTP GET pour récupérer les données et les stocke dans le BehaviorSubject olympics$.
+   * Gère également les erreurs en renvoyant un tableau vide et en loggant l'erreur dans la console.
+   */
   loadInitialData(): Observable<Olympic[]> {
     return this.http.get<Olympic[]>(this.olympicUrl).pipe(
+      // `tap` permet d'effectuer des effets de bord, ici pour mettre à jour le BehaviorSubject avec les données reçues
       tap((value) => this.olympics$.next(value)),
+      // `catchError` permet de capturer les erreurs et d'effectuer des actions spécifiques, ici l'envoi d'un tableau vide dans olympics$
       catchError((error, caught) => {
         // TODO: improve error handling
         console.error(error);
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,7 +10,10 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  // ? Observable contenant la liste des Jeux Olympiques, chaque élément étant de type Olympic[]
   public olympics$!: Observable<Olympic[]>;
+
+  // ? Observable contenant les données formatées pour le graphique en camembert.
   public medalsData$!: Observable<
     {
       name: string;
@@ -18,32 +21,38 @@ export class HomeComponent implements OnInit {
     }[]
   >;
 
+  // ! Observable contenant les données formatées pour le graphique en camembert.
   constructor(private olympicService: OlympicService) {}
 
+  // Initialisation du composant : appel des Observables pour récupérer les données.
   ngOnInit(): void {
+    // Récupération des données des Jeux Olympiques avec plusieurs transformations et gestion des erreurs.
     this.olympics$ = this.olympicService.getOlympics().pipe(
-      delay(2000),
-      map((data) => data || []), // Si data est null ou undefined, retourne un tableau vide
-      catchError(() => of([])) // En cas d'erreur, retourne un tableau vide
+      delay(2000), // Ajoute un délai artificiel de 2 secondes pour simuler le chargement
+      map((data) => data || []), // Transformation des données : retourne un tableau vide si data est null ou undefined
+      catchError(() => of([])) // Gestion des erreurs : retourne un tableau vide en cas d'erreur dans la récupération des données
     );
 
+    // Transformation des données olympiques pour formater les informations de médailles pour le graphique
     this.medalsData$ = this.olympics$.pipe(
       map((olympics: Olympic[]) => {
+        // Mapping des données : chaque élément est transformé en un objet contenant le pays et le total de médailles
         return olympics.map((olympic: Olympic) => {
           return {
-            name: olympic.country,
+            name: olympic.country, // Utilisation du nom du pays comme label dans le graphique
             value: olympic.participations.reduce(
               (acc: number, participation: Participation) =>
-                acc + participation.medalsCount,
+                acc + participation.medalsCount, // Calcul du total de médailles pour chaque pays
               0
             ),
           };
         });
       }),
-      startWith([]) // Commencer avec un tableau vide le temps que les données soient chargées
+      startWith([]) // Commencer avec un tableau vide le temps que les données soient chargées, pour éviter les erreurs de rendu
     );
   }
 
+  // Schéma de couleurs personnalisé pour le graphique en camembert
   colorScheme = {
     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA', '#FF8C00'],
   };
