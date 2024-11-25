1bed6ca8aa11cdf9eb9a1be4d948ad8a22850c1c
NDRBAX <andre.bacaliu@icloud.com>
2024-11-25 13:43:14 +0100

    feat: Add loading and error handling to AppComponent
    - Introduced 'isLoading$' and 'errorMessage$' observables for managing
    loading and error states
    - Updated ngOnInit to handle data loading, updating loading state, and
    handling errors
    - Added loading spinner and error message in the template based on
    observable states
    - Displayed 'app-header' and 'router-outlet' only when data is
    successfully loaded and no error occurs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { take } from 'rxjs';
+import { BehaviorSubject, delay, filter, Observable, take } from 'rxjs';
 import { OlympicService } from './core/services/olympic.service';
 
 @Component({
@@ -8,9 +8,30 @@ import { OlympicService } from './core/services/olympic.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private olympicService: OlympicService) {}
+  public isLoading$ = new BehaviorSubject<boolean>(true);
+  public errorMessage$: Observable<string | null>;
+
+  constructor(private olympicService: OlympicService) {
+    this.errorMessage$ = this.olympicService.getError();
+  }
 
   ngOnInit(): void {
+    // Load initial data
     this.olympicService.loadInitialData().pipe(take(1)).subscribe();
+
+    // Update loading state
+    this.olympicService
+      .getOlympics()
+      .pipe(
+        filter((olympics) => olympics !== null), // Filter out null values
+        take(1), // Take only the first value
+        delay(1000) // Simulate loading time
+      )
+      .subscribe((olympics) => {
+        // If we have data, set loading to false
+        if (olympics !== null && olympics.length > 0) {
+          this.isLoading$.next(false);
+        }
+      });
   }
 }
