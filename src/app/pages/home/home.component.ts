acf4a365ed42642301791da07fdb1ad0c040959f
NDRBAX <andre.bacaliu@icloud.com>
2024-11-25 13:48:26 +0100

    refactor: Simplify HomeComponent logic and update template
    - Removed the loading and error handling logic from the template,
    simplifying the structure
    - Moved the chart display logic directly into the template, removing
    unnecessary conditionals
    - Refactored the component to directly use the OlympicService for
    fetching Olympic data and mapping it to the medals data
    - Removed unnecessary observables for error handling and loading states
    - Simplified data processing for medals chart display

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { catchError, delay, map, Observable, of, startWith } from 'rxjs';
+import { map, Observable, startWith } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
-import { Participation } from 'src/app/core/models/Participation';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -11,31 +10,13 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$!: Observable<Olympic[] | null>;
   public medalsData$!: Observable<{ name: string; value: number }[]>;
-  public error$!: Observable<string | null>;
-  public isLoading = true;
 
   constructor(private olympicService: OlympicService, private router: Router) {}
 
   ngOnInit(): void {
-    // Charge les données olympiques et gère l'état de chargement et d'erreur
-    this.olympicService.loadInitialData().subscribe({
-      complete: () => (this.isLoading = false),
-      error: () => (this.isLoading = false),
-    });
-
-    // Observable pour récupérer les Jeux Olympiques
-    this.olympics$ = this.olympicService.getOlympics().pipe(
-      map((data) => data || []), // Retourne un tableau vide si data est null ou undefined
-      catchError(() => of([])) // Retourne un tableau vide en cas d'erreur
-    );
-
-    // Observable pour récupérer les erreurs depuis le service
-    this.error$ = this.olympicService.getError();
-
-    // Transformation des données pour le graphique
-    this.medalsData$ = this.olympics$.pipe(
+    this.medalsData$ = this.olympicService.getOlympics().pipe(
+      startWith(null),
       map((olympics: Olympic[] | null) => {
         return olympics
           ? olympics.map((olympic: Olympic) => ({
@@ -46,8 +27,7 @@ export class HomeComponent implements OnInit {
               ),
             }))
           : [];
-      }),
-      startWith([])
+      })
     );
   }
 
